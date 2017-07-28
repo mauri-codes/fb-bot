ef958217edbd82aaae9cb0816ac9c8c574d41912
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2017-07-28 21:42:23 +0000

    many answers are sent to facebook

--- index.js
+++ index.js
@@ -13,36 +13,45 @@ app.get('/', function(req, res){
   res.send('Hi chatbot is here once again!')
 })
 
-app.get('/webhook/', function(req, res){
-  if(req.query['hub.verify_token'] === 'thompson54'){
-    res.send(req.query['hub.challenge'])
-  }
-  res.send("Wrong token")
-})
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var messages = [{"message1": "Hola ", "message2": ". Nos gustaria que nos compartas tu correo para que te enviemos mas informacion."},
+                {"message1": "", "message2": " por favor facilitanos tu correo electronico, te enviaremos mas informacion"},
+                {"message1": "", "message2": ", nos gustaria mandarte un correo para mandarte mayores detalles"},
+                {"message1": "Hola ", "message2": ", nos podrias facilitar tu correo para enviarte mas informacion al respecto?"},
+                {"message1": "", "message2": ", por favor compartenos tu correo electronico, te compartiremos mas informacion"}
+                ];
 
 app.post('/webhook/', function(req, res) {
-  var fb_entry   = req.body.entry;
-  var entry_id   = fb_entry[0].time;
-  var changes    = fb_entry[0].changes;
-  var field      = changes[0].field;//feed
-  var item_type  = changes[0].value.item;//comment
-  var sender     = changes[0].value.sender_name;
-  var comment_id = changes[0].value.comment_id;
-  var post_id    = changes[0].value.post_id;
-  var message    = changes[0].value.message;
-  console.log("message: " + message);
-  console.log("post id: " + post_id);
-  console.log("sender: "  + sender);
-  console.log("comment_id: " + comment_id);
-  sendText(sender, comment_id);
-  console.log('Everything looks fine');
-  res.sendStatus(200);
+    var fb_entry   = req.body.entry;
+    var entry_id   = fb_entry[0].time;
+    if(fb_entry[0].changes){
+        var changes    = fb_entry[0].changes;
+        var field      = changes[0].field;//feed
+        if(field == "feed"){
+            var item_type  = changes[0].value.item;//comment
+            if(item_type == "comment"){
+                var sender     = changes[0].value.sender_name;
+                var comment_id = changes[0].value.comment_id;
+                var post_id    = changes[0].value.post_id;
+                var message    = changes[0].value.message;
+                sendText(sender, comment_id);
+                console.log('Everything looks fine');
+            }
+        }
+    }
+
+    res.sendStatus(200);
 });
+
 
 var token = "EAALp6npgAb0BADEZB6unhZCw4uZCerwQPC5OlcU13ixY5mXU0swqUh1muihCUdWBw7QUIj5uOffUjCDY3PyoSNjtJmOHQmFd59mif7SHDmE4w9nJG3LDYH8WvJRlnMltSxbhszVMsqbZAWZBDmi9CAt1P0qvnxMZAjo58WXesGOwZDZD";
 
 function sendText(sender_name, comment_id){
-    var  messageData = "hola " + sender_name + ". Por favor facilitanos tu correo para enviarte mas información.";
+    var randomNumber = random(0,4);
+    var  messageData = messages[randomNumber].message1 + sender_name + messages[randomNumber].message2;
     request({
         url: "https://graph.facebook.com/" + comment_id + "/comments",
         qs: {access_token: token},
